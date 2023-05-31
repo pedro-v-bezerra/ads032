401d1cb8d18efb70677efe0cadb19a2b507e7783
Pedro Lima <pedrolima38@hotmail.com>
2023-05-30 21:21:21 -0300

    pratica03

--- pratica02/src/pages/Home.jsx
+++ pratica02/src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import ContatosContext from "../context/ContatosContext";
+
 export default function Home() {
+  const {meusContatos} = useContext(ContatosContext);
   return (
     <>
       <h2>Meus Contatos</h2>
+      <ul>
+        {meusContatos.map((contato, key) => (
+          <li id={key}>
+            {contato.nome} - {contato.telefone}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
--- pratica02/src/pages/Novo.jsx
+++ pratica02/src/pages/Novo.jsx
@@ -1,7 +1,56 @@
+import { useForm } from "react-hook-form";
+import { useContext } from "react";
+import ContatosContext from "../context/ContatosContext";
+import { useNavigate } from "react-router-dom";
+
 export default function Novo() {
+  const navigate = useNavigate()
+  const {incluirContato} = useContext(ContatosContext)
+  const form = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = form;
+
+  const validaNome = {
+    required: {
+      value: true,
+      message: "Campo Obrigatório",
+    },
+  };
+  const validaTelefone = {
+    required: {
+      value: true,
+      message: "Campo Obrigatório",
+    },
+  };
+
+  function onSubmit(data) {
+    incluirContato(data)
+    navigate("/")
+  }
+
   return (
     <>
       <h2>Novo Contato</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="Nome">Nome</label>
+          <input type="name" id="nome" {...register("nome", validaNome)} />
+          {errors.nome && <p>{errors.nome.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="Telefone">Telefone</label>
+          <input
+            type="number"
+            id="telefone"
+            {...register("telefone", validaTelefone)}
+          />
+          {errors.telefone && <p>{errors.telefone.message}</p>}
+        </div>
+        <button>Salvar</button>
+      </form>
     </>
   );
 }
